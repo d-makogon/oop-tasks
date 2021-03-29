709444f8461e119a5499950c8175b73a5b788fc2
Dmitriy Makogon <d.makogon@g.nsu.ru>
2021-03-29 18:26:23 +0700

    store classes instead of strings in factory

--- java-task2/src/main/java/ru/nsu/fit/g19202/dmakogon/task2/calc/CommandsFactory.java
+++ java-task2/src/main/java/ru/nsu/fit/g19202/dmakogon/task2/calc/CommandsFactory.java
@@ -3,12 +3,13 @@ package ru.nsu.fit.g19202.dmakogon.task2.calc;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.nsu.fit.g19202.dmakogon.task2.calc.commands.Command;
+import ru.nsu.fit.g19202.dmakogon.task2.calc.exceptions.CommandNotFoundException;
 import ru.nsu.fit.g19202.dmakogon.task2.calc.exceptions.InvalidCommandException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.PropertyResourceBundle;
 
@@ -16,52 +17,29 @@ public class CommandsFactory
 {
     private final static Logger logger = LogManager.getLogger();
 
-    private static final String COMMANDS_PROP = "/commands.properties";
-    private final HashMap<String, String> commandsClassesNames;
+    private static final String COMMANDS_PROP = "commands.properties";
+    private final Map<String, Class<?>> commandsClasses = new HashMap<>();
 
-    public CommandsFactory()
+    public CommandsFactory() throws CommandNotFoundException, InvalidCommandException, IOException
     {
         logger.trace("Constructing new CommandsFactory...");
 
-        commandsClassesNames = new HashMap<>();
+        logger.info("Reading properties from '" + COMMANDS_PROP + "'");
 
-        InputStream propIn = getClass().getResourceAsStream(COMMANDS_PROP);
+        InputStream propsInputStream = getClass().getResourceAsStream(COMMANDS_PROP);
 
-        if (propIn == null)
+        if (propsInputStream == null)
         {
             String msg = "Unable to find '" + COMMANDS_PROP + "' bundle.";
             logger.fatal(msg);
             throw new MissingResourceException(msg, COMMANDS_PROP, "");
         }
 
-        PropertyResourceBundle bundle;
-        try
-        {
-            bundle = new PropertyResourceBundle(new InputStreamReader(propIn));
-        }
-        catch (IOException e)
-        {
-            String msg = "Unable to open '" + COMMANDS_PROP + "' bundle.";
-            logger.fatal(msg);
-            throw new MissingResourceException(msg, COMMANDS_PROP, "");
-        }
-
-        logger.info("Reading keys from " + COMMANDS_PROP + ".");
-        var propKeys = bundle.getKeys();
-
-        int count = 0;
-        while (propKeys.hasMoreElements())
-        {
-            String key = propKeys.nextElement();
-            commandsClassesNames.put(key.toLowerCase(), bundle.getString(key));
-            count++;
-        }
-
-        logger.info("Successfully read " + count + " properties.");
+        readProperties(propsInputStream);
 
         try
         {
-            propIn.close();
+            propsInputStream.close();
         }
         catch (IOException e)
         {
@@ -69,34 +47,71 @@ public class CommandsFactory
         }
     }
 
-    public Command create(String commandName) throws InvalidCommandException
+    public CommandsFactory(InputStream propsInputStream) throws InvalidCommandException, CommandNotFoundException, IOException
+    {
+        logger.trace("Constructing new CommandsFactory...");
+        logger.info("Reading properties from " + propsInputStream.getClass().getSimpleName());
+
+        readProperties(propsInputStream);
+    }
+
+    private void readProperties(InputStream propsInputStream) throws CommandNotFoundException, InvalidCommandException, IOException
+    {
+        PropertyResourceBundle bundle = new PropertyResourceBundle(propsInputStream);
+
+        var propKeys = bundle.getKeys();
+
+        int count = 0;
+        while (propKeys.hasMoreElements())
+        {
+            String key = propKeys.nextElement();
+            String className = bundle.getString(key);
+
+            Class<?> cl = null;
+            try
+            {
+                cl = Class.forName(className);
+            }
+            catch (ClassNotFoundException e)
+            {
+                throw new CommandNotFoundException("Unable to find " + className + " class");
+            }
+
+            if (!Command.class.isAssignableFrom(cl))
+            {
+                throw new InvalidCommandException(className + " class doesn't derive from " + Command.class.getName());
+            }
+            commandsClasses.put(key.toLowerCase(), cl);
+            count++;
+        }
+
+        logger.info("Successfully read " + count + " properties.");
+
+    }
+
+    public Command create(String commandName) throws InvalidCommandException, CommandNotFoundException
     {
         logger.trace("Trying to create Command with name '" + commandName + "'.");
-        Command instance;
+        Command instance = null;
 
-        String className = commandsClassesNames.get(commandName.toLowerCase());
-        if (className == null)
+        var commandClass = commandsClasses.get(commandName.toLowerCase());
+        if (commandClass == null)
         {
             logger.error("Unable to find class name for command name '" + commandName + "'.");
-            throw new InvalidCommandException("Unknown command: " + commandName);
+            throw new CommandNotFoundException("Unknown command: " + commandName);
         }
+
         try
         {
-            instance = (Command) Class.forName(className).getDeclaredConstructor().newInstance();
+            instance = (Command) commandClass.getDeclaredConstructor().newInstance();
         }
-        catch (ClassNotFoundException e)
+        catch (ReflectiveOperationException e)
         {
-            logger.error("Unable to find class for name '" + className + "'.");
+            logger.error("Unable to create instance of " + commandClass.getName() + " class");
             throw new InvalidCommandException("Unknown command: " + commandName);
-        }
-        catch (Exception e)
-        {
-            logger.error("Unable to create new instance of Command with name '" + commandName + "'.\n" + e.getMessage());
-            throw new RuntimeException("Error creating Command: " + e.toString());
         }
 
         logger.info("Successfully created Command instance with name '" + commandName + "'.");
-
         return instance;
     }
 }
